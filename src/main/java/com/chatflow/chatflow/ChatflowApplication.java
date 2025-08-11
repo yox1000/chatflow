7ec35fab0d947f8ca814f733bf6c7fed77146b5c
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2025-08-11 19:05:05 -0400

    file configuration

---
+++ src/main/java/com/chatflow/chatflow/ChatflowApplication.java
@@ -0,0 +1,13 @@
+package com.chatflow.chatflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatflowApplication.class, args);
+	}
+
+}
